5c915244299438950d1da1f3ddfbfd72c4335179
Simon Lee <simonlee98853@gmail.com>
2023-01-05 13:28:31 -0500

    fixed logic of checkWinner and added gameController module

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 
 const gameBoard = (() => {
-  let array = ["O", "", "", "", "", "", "", "", ""];
+  let array = ["", "", "", "", "", "", "", "", ""];
   const getArray = () => array;
   const setArray = (index, side) => {
     if (array[index] == "") {
@@ -16,7 +16,7 @@ const gameBoard = (() => {
       gridItem.classList.add("grid-item");
       gridItem.setAttribute("data-index", `${i}`);
       gridItem.textContent = array[i];
-      gridItem.addEventListener("click", playRound);
+      gridItem.addEventListener("click", gameController.playRound);
       gridContainer.appendChild(gridItem);
     }
   };
@@ -38,84 +38,102 @@ const gameBoard = (() => {
 })();
 
 const Player = (name, side, playing) => {
+  let isTurn = playing;
   const getName = () => name;
   const getSide = () => side;
-  const getPlaying = () => playing;
+  const getPlaying = () => isTurn;
 
   const play = (e) => {
     let index = e.target.dataset.index;
     gameBoard.setArray(index, side);
   };
   const endTurn = () => {
-    this.playing = !playing;
+    isTurn = !isTurn;
   };
   return { play, endTurn, getName, getSide, getPlaying };
 };
 const player1 = Player("player1", "X", true);
 const player2 = Player("player2", "O", false);
 
-function playRound(e) {
-  if (!checkWinner(gameBoard.getArray())) {
-    if (player1.getPlaying()) {
-      player1.play(e);
-      gameBoard.resetGameBoard();
-      gameBoard.displayGameBoard();
-      player1.endTurn();
-      player2.endTurn();
+const gameController = (() => {
+  function playRound(e) {
+    if (!checkWinner(gameBoard.getArray())) {
+      if (player1.getPlaying()) {
+        player1.play(e);
+        gameBoard.resetGameBoard();
+        gameBoard.displayGameBoard();
+        let isWinner = checkWinner(gameBoard.getArray());
+        if (isWinner != false) {
+          displayWinner(isWinner);
+        }
+        player1.endTurn();
+        player2.endTurn();
+      } else {
+        player2.play(e);
+        gameBoard.resetGameBoard();
+        gameBoard.displayGameBoard();
+        let isWinner = checkWinner(gameBoard.getArray());
+        if (isWinner != false) {
+          displayWinner(isWinner);
+        }
+        player1.endTurn();
+        player2.endTurn();
+      }
     } else {
-      player2.play(e);
-      gameBoard.resetGameBoard();
-      gameBoard.displayGameBoard();
-      player1.endTurn();
-      player2.endTurn();
+      console.log("bob");
     }
-  } else {
-    if (checkWinner(gameBoard.getArray()) == "DRAW") {
+  }
+  const displayWinner = (winner) => {
+    if (winner == "DRAW") {
       console.log("draw");
-    } else if (checkWinner(gameBoard.getArray()) == "X") {
+    } else if (winner == "X") {
       console.log("X wins");
     } else {
       console.log("O wins");
     }
-  }
-}
-
-const checkWinner = (array) => {
-  // checks rows
-  for (let i = 0; i < 3; i++) {
-    let row = [];
-    for (let j = i * 3; j < i * 3 + 3; j++) {
-      row.push(array[j]);
-    }
-    if (row.every(field => field == 'X') || row.every(field => field == 'O')) {
+  };
+  const checkWinner = (array) => {
+    // checks rows
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = i * 3; j < i * 3 + 3; j++) {
+        row.push(array[j]);
+      }
+      if (
+        row.every((field) => field == "X") ||
+        row.every((field) => field == "O")
+      ) {
         return row[0];
+      }
     }
-  }
-  //checks columns
-  for (let i = 0; i < 3; i++) {
-    let column = [];
-    for (let j = 0; j < 3; j++) {
-      column.push(array[i + 3 * j]);
-    }
-    if (column.every(field => field == 'X') || column.every(field => field == 'O')) {
+    //checks columns
+    for (let i = 0; i < 3; i++) {
+      let column = [];
+      for (let j = 0; j < 3; j++) {
+        column.push(array[i + 3 * j]);
+      }
+      if (
+        column.every((field) => field == "X") ||
+        column.every((field) => field == "O")
+      ) {
         return column[0];
+      }
     }
-  }
-  //checks diagnals
-  if(array[0] != '' && array[0] == array[4] && array[4] == array[8]){
-    return array[0];
-  }
-  if(array[2] != '' && array[2] == array[4] && array[4] == array[6]){
-    return array[0];
-  }
-  //checks draw
-  for(let i=0; i<9;i++){
-    if(array[i] == ''){
-        return false;
+    //checks diagnals
+    if (array[0] != "" && array[0] == array[4] && array[4] == array[8]) {
+      return array[0];
     }
-  }
-  return 'DRAW';
-};
+    if (array[2] != "" && array[2] == array[4] && array[4] == array[6]) {
+      return array[0];
+    }
+    //checks draw
+    if (array.every((field) => field != "")) {
+      return "DRAW";
+    }
+    return false;
+  };
+  return {playRound};
+})();
 window.onload = () => {
   gameBoard.displayGameBoard();
 };
